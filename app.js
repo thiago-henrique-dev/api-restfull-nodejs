662bee44b91e75ac6a3d1f5e1aafa193391d9907
thiago <thiago.monteiro.clear@gmail.com>
2022-03-11 10:45:23 -0300

    melhorias e tratamentos de errors

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 
 const rotasProdutos = require('./routes/produtos');
 const rotasPedidos = require('./routes/pedidos')
 
+app.use(morgan('dev'));
 app.use('/produtos', rotasProdutos);
 app.use('/pedidos', rotasPedidos)
 
+// Quando nao encontra uma rota...
+app.use((req, res, next) => {
+    const erro = new Error('Nao encontrado');
+    erro.status = 404;
+    next(erro)
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+});
 
 module.exports = app;
