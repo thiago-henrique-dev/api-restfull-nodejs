39bca02efaae51e00fced6c56198402f7a420c81
thiago <thiago.monteiro.clear@gmail.com>
2022-03-14 11:55:40 -0300

    aplicando as alteracoes nas rotas pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,45 +1,136 @@
 const express = require('express');
 const router = express.Router()
+const mysql = require('../mysql').pool;
 
 //Retorna todos os pedidos
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Retorna todos os produtos'
+    mysql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error:error})}
+        conn.query(`SELECT * FROM pedidos;`,
+            (error, result, fields) => {
+                if(error){ return res.status(500).send({eror:error})}
+                    conn.release()
+                        if(error){return res.status(500).send({error:error})}
+                            const response = {
+                                quantidade: result.length,
+                                pedidos: result.map(pedidos => {
+                                    return {
+                                        id_pedido: pedidos.id_pedido,
+                                        id_produto: pedidos.id_produto,
+                                        quantidade: pedidos.quantidade,
+                                        request:{
+                                            tipo: "GET",
+                                            descricao: `Retorna todos os pedidos`
+                                        }
+                                    }
+                                })
+                            }
+                            return res.status(200).send(response)
+            })
     })
 });
 
 //Inseri um pedido
 router.post('/', (req, res, next) => {
-    const pedido = {
-        id_produto: req.body.id_produto,
-        quantidade: req.body.quantidade
-    }
-    res.status(201).send({
-        mensagem: 'O pedido foi criado',
-        pedidoCriado: pedido
+    mysql.getConnection((error, conn) => {
+        if (error) {return res.status(500).send({error: error})}
+        conn.query(
+            `SELECT * FROM produtos WHERE id_produto = ?`, [req.body.id_produto], // VERIFICO SE TEM PRODUTO
+                (error, result, fields) => {
+                    if (error) {return res.status(500).send({error: error})}
+                    if(result.length == 0){
+                        return res.status(404).send({  // SE NAO TIVER PRODUTOS ELE RETORNA STATUS 404
+                            mensagem: `Produto nao encontrado`
+                        })
+                    } // CASO TENHA PRODUTOS ELE NAO ENTRA NO IF.. E VAI DAR CONTINUIDADE PARA INSERIR O PEDIDO 
+                    mysql.getConnection((error, conn) => {
+                        if (error) {return res.status(500).send({error: error})}
+                        conn.query('INSERT INTO pedidos (id_produto, quantidades) VALUES (?,?)',
+                            [req.body.id_produto, req.body.quantidades],
+                                (error, result, field) => {
+                                    conn.release()
+                                        if(error) { return res.status(500).send({ error: error})}
+                                            const response = {
+                                                mensagem: 'Pedido inserido com sucesso',
+                                                produtoCriado: {
+                                                    id_pedido: result.id_pedidos,
+                                                    id_produto: req.body.id_produto,
+                                                    quantidade: req.body.quantidades,
+                                                    request: {
+                                                        tipo: 'GET',
+                                                        descricao: 'Insere um pedido',
+                                                        url: 'http://localhost:3000/pedidos'
+                                                    }
+                                                }
+                                            }
+                                            return res.status(201).send(response)
+                                }
+                        
+                        )
+                    })
+                });
+                
+                //Retorna os dados de um pedido
+                router.get('/:id_pedidos', (req, res, next) => {
+                        mysql.getConnection((error, conn) => {
+                            if(error) {return res.status(500).send({error: error})}
+                                conn.query(
+                                    `SELECT * FROM pedidos WHERE id_pedidos = ?;`,
+                                    [req.params.id_pedidos],
+                                        (error, result, field) => {
+                                            if(result.length == 0){
+                                                res.status(404).send({
+                                                    mensagem: `Nao foi encontrado pedido com este ID`
+                                                })
+                                            }
+                                                const response = {
+                                                    pedido: {
+                                                        id_pedidos: result[0].id_pedidos,
+                                                        id_produto: result[0].id_produto,
+                                                        quantidades: result[0].quantidades,
+                                                        request: {
+                                                            tipo: 'GET',
+                                                            descricao: 'Retorna todos os pedidos',
+                                                            url: 'http://localhost:3000/pedidos'
+                                                        }
+                                                    }
+                                                }
+                                                return res.status(200).send(response)
+                
+                                        }
+                                )
+                        })
+
+          })
     })
-});
 
-//Retorna os dados de um pedido
-router.get('/:id_pedido', (req, res, next) => {
-        const id = req.params.id_pedido
-
-            res.status(200).send({
-            mensagem: 'Detalhes do pedido',
-            id: id
-        });    
-});
-
-router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Produto alterado'
-    })
+     
 })
 
-//Exclui um pedido
 router.delete('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Excluindo um pedido'
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error: error})}
+            conn.query(
+                `DELETE FROM pedidos WHERE id_pedidos = ?`,
+                    [req.body.id_pedidos],
+                        (error, result, field) => {
+                            conn.release();
+                                if(error) {return res.status(500).send({error: error})}
+                                    const response = {
+                                        mensagem: `Pedido removido com sucesso`,
+                                        request:{
+                                            tipo: `DELETE`,
+                                            descricao: `Deletando um pedido`,
+                                            url: `http://localhost:3000/pedidos`,
+                                            body: {
+                                                id_produto: `Number`,
+                                                quantidade: `Number`
+                                            }
+                                        }
+                                    }
+                                    return res.status(202).send(response)
+                        }
+            )
     })
 })
 
