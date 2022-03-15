cb1ca51e0106bb9821fc08fc76a79c0a24a8547a
thiago <thiago.monteiro.clear@gmail.com>
2022-03-15 10:24:27 -0300

    login + jwt token...

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'),
     res.header(
             'Access-Control-Allow-Header',
-             'Origin, X-Requrested-With, Content-Type, Authorization'
+             'Origin, X-Requested-With, Content-Type, Accept, Authorization'
              );
     if(req.method === 'OPTIONS'){
-        res.heade('Acess-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
         return res.status(200).send({})
     }
     next();
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router()
 const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt'); //biblioteca para criptografar a senha 
+const jwt = require('jsonwebtoken');
 
 router.post('/cadastro', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         conn.query('SELECT * FROM usuarios WHERE email = ?', [req.body.email], (error, results, fields) => {
             if(error){ return res.status(500).send({error: error})}
             if(results.length > 0){
-                res.status(409).send({ mensagem: "Usuario ja cadastrado"})
+                res.status(409).send({ mensagem: "Usuario ja cadastrado"}) 
             } else {
                 bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
                     if(errBcrypt){ return res.status(500).send({ error: errBcrypt }) }
@@ -34,5 +35,38 @@ router.post('/cadastro', (req, res, next) => {
     })
 })
 
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+          const query = `SELECT * FROM usuarios WHERE email = ?`;
+                conn.query(query,[req.body.email],(error, results, fields) => {
+                    conn.release();
+                       if (error) { return res.status(500).send({ error: error }) }
+                            if (results.length < 1) {
+                                return res.status(401).send({ mensagem: 'Falha na autenticação' })
+            }
+                                      bcrypt.compare(req.body.senha, results[0].senha, (err, result) => {
+                                          if (err) {
+                                             return res.status(401).send({ mensagem: 'Falha na autenticação' })
+                            }
+                                                if (result) {
+                                                     const token = jwt.sign({
+                                                          id_usuario: results[0].id_usuario,
+                                                             email: results[0].email
+                                                          },
+                                                              process.env.JWT_KEY,
+                                                            {
+                                                                expiresIn: "1h"
+                                                            });
+                                                                return res.status(200).send({
+                                                                    mensagem: 'Autenticado com sucesso',
+                                                                    token: token
+                                                                        });
+                                                                           }
+                                                                     return res.status(401).send({ mensagem: 'Falha na autenticação' })
+                                                        });
+                                                    });
+                                                });
+                                            })
 
 module.exports = router;
